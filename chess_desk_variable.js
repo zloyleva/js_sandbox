1093eb504c8f00a3876bea0f532223a49f026988
zloyleva <zloyleva@gmail.com>
2016-11-25 22:42:40 +0300

    add variable chess desk with validation

---
+++ chess_desk_variable.js
@@ -0,0 +1,16 @@
+﻿var num = prompt("Text chess desk size: ")
+
+if (!isNaN(num)){
+	for(var i = 1;i <= num; i++){
+		if(i%2 == 0) line(" #");
+		else line("# ");
+	}
+
+	function line(item){
+		var string = "";
+		for (var i = 0; i < num; i++) {
+			string += item;
+		}
+		console.log(string);
+	}
+}else alert("You text not a number!");
