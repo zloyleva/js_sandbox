e6600cb8a7a6c198d6967a398f392a1562da4216
zloyleva <zloyleva@gmail.com>
2016-11-25 22:40:12 +0300

    Add FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+﻿for(var i = 1;i<101;i++){
+	console.log(i + " : " + fizzBuzz(i));
+}
+
+function fizzBuzz(number){
+	if(i%3 == 0) if(i%5 == 0) return "FizzBuzz";
+	else return "Fizz";
+	if(i%5 == 0) return "Buzz";
+	return "";
+}
