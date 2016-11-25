a9756ed82a8506f0e53987db5122c0e380bf6d3f
zloyleva <zloyleva@gmail.com>
2016-11-25 22:42:11 +0300

    Add chess desk 8*8

---
+++ chess_8_8.js
@@ -0,0 +1,12 @@
+﻿for(var i = 1;i <= 8; i++){
+	if(i%2 == 0) line(" #");
+	else line("# ");
+}
+
+function line(item){
+	var string = "";
+	for (var i = 0; i < 8; i++) {
+		string += item;
+	}
+	console.log(string);
+}
