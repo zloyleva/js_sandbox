ca55b0e0f380637e0cf96f03f60388254215f799
zloyleva <zloyleva@gmail.com>
2016-12-04 00:29:45 +0300

    Add parser ABC jr numbers

---
+++ show_me_abc.js
@@ -0,0 +1,13 @@
+﻿function abcString(aInd, cont, callback){
+	var abc = "";
+	for (var i = 0; i < cont; i++) {
+		abc += callback(aInd, i) + " ";
+	}
+	return abc;
+}
+
+// To show me the ABC
+document.write(abcString(97, 26, function(ind, index){return String.fromCharCode(ind + index);}) + "<br/>");
+
+// To show me numbers 0-9
+document.write(abcString(0, 10, function(ind, index){return ind + index;}));
